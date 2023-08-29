096486abda7e344d4b870e69e599d126d56db07f
akhanafer <akhanafer@kinaxis.com>
2023-08-29 06:35:06 -0400

    Initial Commit

---
+++ Wearable Sensor Watch App/Wearable_SensorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wearable_SensorApp.swift
+//  Wearable Sensor Watch App
+//
+//  Created by akhanafer on 2023-08-29.
+//
+
+import SwiftUI
+
+@main
+struct Wearable_Sensor_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
